2ad71914274db1399a24509e068ffdbbecb9b012
“aezazi” <aezazi@icloud.com>
2025-10-11 21:08:43 +0000

    implementing training on gpu

--- sft_train.py
+++ sft_train.py
@@ -12,6 +12,20 @@ model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1")
 # load the saved custom tokenizer
 tokenizer = AutoTokenizer.from_pretrained("./tokenizer_with_specials")
 
-# resize the vocab size of the model to match the modified tokenizer
-model.resize_token_embeddings(len(tokenizer))
 
+# %%
+# load the templated dataset
+from datasets import load_from_disk
+dataset_templated = load_from_disk("./dataset_ultrachat_train_sft_templated")
+# %%
+print(dataset_templated.column_names)
+print(dataset_templated['templated_msgs'][0])
+# %%
+
+
+# %%
+# Save the resized model
+output_dir = "./mistral-7b-resized"
+
+# Save the model
+model.save_pretrained(output_dir)
